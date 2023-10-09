d5b6d0a33f9743455a43af92ac70c4a1d7987f87
Its Amiracle <122039464+amiracle1337@users.noreply.github.com>
2023-10-09 21:37:21 +0200

    Update env.js

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 export const process = {
     env: {
-        OPENAI_API_KEY: "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",
-        ELEVEN_LABS_API_KEY: "004d167cf07baf44ec3c955d5304d114"
+        OPENAI_API_KEY: "YOUR API KEY",
+        ELEVEN_LABS_API_KEY: "YOUR API KEY"
     }
 }
